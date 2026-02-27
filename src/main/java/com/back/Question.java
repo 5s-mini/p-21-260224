1ae167bb033fb7cc2070b6e3e00916e9a2459387
5s-mini <dnclsehd123@naver.com>
2026-02-27 11:01:20 +0900

    feat: JPA에서 특정 글에 관련된 질문들을 조회하는 로직 추가

--- src/main/java/com/back/Question.java
+++ src/main/java/com/back/Question.java
@@ -1,11 +1,10 @@
 package com.back;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Getter;
 
 @Entity
+@Getter
 public class Question {
 
     @Id
